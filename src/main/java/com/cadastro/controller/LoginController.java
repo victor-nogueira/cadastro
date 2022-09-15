fb94133f6d8881eaf6b316b37d7bc7496b390214
victor-nogueira <jvnr2011@gmail.com>
2022-09-15 16:46:15 -0300

    pagina login

--- src/main/java/com/cadastro/controller/LoginController.java
+++ src/main/java/com/cadastro/controller/LoginController.java
@@ -1,5 +1,15 @@
 package com.cadastro.controller;
 
-public class LoginController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class LoginController {
+	
+	@GetMapping("/login")
+	public String login () {
+		return "login";
+	}
+	
 }
