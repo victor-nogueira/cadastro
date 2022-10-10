9cab9f4ce2662d04dcbe053f7bf98fb812de3ef3
victor-nogueira <jvnr2011@gmail.com>
2022-10-10 09:36:26 -0300

    security implements

--- src/main/java/com/cadastro/security/NavController.java
+++ src/main/java/com/cadastro/security/NavController.java
@@ -1,7 +1,9 @@
 package com.cadastro.security;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class NavController {
 	
 	@RequestMapping("/auth")
