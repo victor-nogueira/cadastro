c42ea8149fa0f6bc8e066ee9961592aa12fb7c7e
victor-nogueira <jvnr2011@gmail.com>
2022-10-10 10:41:56 -0300

    security implements

--- src/main/java/com/cadastro/security/SecurityConfig.java
+++ src/main/java/com/cadastro/security/SecurityConfig.java
@@ -4,11 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -39,5 +43,6 @@ public class SecurityConfig {
 	public PasswordEncoder passwordEncoder () {
 		return new BCryptPasswordEncoder();
 	}
+	
 	
 }
